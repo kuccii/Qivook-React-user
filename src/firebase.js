f514d940e1d5df3d4d1b6bd831587f6a5311f1a1
Patrick Hirwa <34752778+kuccii@users.noreply.github.com>
2025-03-01 03:24:00 +0200

    main app to server

---
+++ src/firebase.js
@@ -0,0 +1,65 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import {
+  getMessaging,
+  getToken,
+  onMessage,
+  isSupported,
+} from "firebase/messaging";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDk7TPyEKRWG0NuJaCoXGXVYcmObylBbUs",
+  authDomain: "primeki-782ff.firebaseapp.com",
+  projectId: "primeki-782ff",
+  storageBucket: "primeki-782ff.appspot.com",
+  messagingSenderId: "299566482846",
+  appId: "1:299566482846:web:80e3f0319c4e8ba9c9428f",
+  measurementId: "G-3HB0X7RZPM"
+};
+const firebaseApp = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
+const messaging = (async () => {
+  try {
+    const isSupportedBrowser = await isSupported();
+    if (isSupportedBrowser) {
+      return getMessaging(firebaseApp);
+    }
+    return null;
+  } catch (err) {
+    return null;
+  }
+})();
+
+export const fetchToken = async (setTokenFound, setFcmToken) => {
+  return getToken(await messaging, {
+    vapidKey: "",
+  })
+    .then((currentToken) => {
+      if (currentToken) {
+        setTokenFound(true);
+        setFcmToken(currentToken);
+
+        // Track the token -> client mapping, by sending to backend server
+        // show on the UI that permission is secured
+      } else {
+        setTokenFound(false);
+        setFcmToken();
+        // shows on the UI that permission is required
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      // catch error while creating client token
+    });
+};
+
+export const onMessageListener = async () =>
+  new Promise((resolve) =>
+    (async () => {
+      const messagingResolve = await messaging;
+      onMessage(messagingResolve, (payload) => {
+        resolve(payload);
+      });
+    })()
+  );
+export const auth = getAuth(firebaseApp);
